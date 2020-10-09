870839e4bd1f5451e693b42e0e4cab3f67083ec2
Jonathan Canela <jonathancanela365@gmail.com>
2020-10-08 18:35:02 -0700

    Finished MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,41 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  let enhancement = 0; 
+
+  if(item.enhancement >= 20){
+    enhancement = 20;
+  } else {
+    enhancement = item.enhancement + 1;
+  }
+
+  return {...item,
+    enhancement: enhancement
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  let durability = 0;
+  let enhancement = 0;
+
+  if(item.enhancement < 15){
+    durability = item.durability - 5;
+  } else if(item.enhancement >= 15) {
+    durability = item.durability - 10;
+  } else if(item.enhancement > 16){
+    enhancement = item.enhancement - 1;
+  }
+
+  return { ...item,
+    durability: durability,
+    enhancement: enhancement
+  };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item,
+          durability: 100
+         };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,49 @@
-const enhancer = require('./enhancer.js');
+const {success, fail, repair} = require('./enhancer.js');
 // test away!
+
+test('success function', () => {
+    const successValue = {
+        name: `Dorian's Blade`,
+        durability: 100,
+        enhancement: 20
+    };
+    const successExpected = {
+        name: `Dorian's Blade`,
+        durability: 100,
+        enhancement: 20
+    };
+
+    expect(success(successValue)).toStrictEqual(successExpected);
+})
+
+test('fail function', () => {
+    const value = {
+        name: 'iron sword',
+        durability: 100,
+        enhancement: 16
+    }; 
+
+    const expected = {
+        name: 'iron sword',
+        durability: 90,
+        enhancement: 0
+    };
+
+    expect(fail(value)).toStrictEqual(expected);
+})
+
+test('repair function', () => {
+    const value = {
+        name: `Luden's Echo`,
+        durability: 20,
+        enhancement: 0
+    };
+    
+    const expected = {
+        name: `Luden's Echo`,
+        durability: 100,
+        enhancement: 0
+    };
+
+    expect(repair(value)).toStrictEqual(expected);
+})
